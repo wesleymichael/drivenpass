ffb2bdee79e4b78f77b8b631bbaf01568a8ea963
Wesley Michael <wesleymichaelpereira@hotmail.com>
2023-08-30 05:35:02 -0300

    feat: add register page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import type { AppProps } from 'next/app';
 import { Header } from '@/components/Header';
 import '../styles/reset.scss';
 import '../styles/global.scss';
+import { useRouter } from 'next/router';
  
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const currentRouterPage = router.pathname;
+
   return (
     <>
-      <Header />
-      <Component {...pageProps} />   
+      {currentRouterPage !== '/register' && <Header />}
+      <Component {...pageProps} /> 
     </>
   )
 }
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import styles from './styles.module.scss';
+import { GiPadlock } from 'react-icons/gi';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+export default function Register() {
+  const [form, setForm] = useState({'email': '', 'password': ''});
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter()
+
+  // if(isLoading) {
+  //   router.push('/')
+  // }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setIsLoading(true);
+  }
+
+  function validateEmail(email: string) {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return emailRegex.test(email);
+  }
+
+  function validatePassword(password: string) {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{10,}$/;
+    return passwordRegex.test(password);
+  }
+
+  
+  return(
+    <main className={styles.registerContainer}>
+      <div className={styles.logoContainer}>
+        <GiPadlock/>
+        <h1>DrivenPass</h1>
+      </div>
+
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          value={form.email}
+          disabled={isLoading}
+          required
+          pattern={validateEmail(form.email) ? undefined : "[^\\s]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"}
+          title="Insira um email válido"
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          name="password"
+          onChange={handleChange}
+          value={form.password}
+          disabled={isLoading}
+          required
+          pattern={validatePassword(form.password) ? undefined : "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{10,}$"}
+          title="A senha deve ter pelo menos: 10 caracteres, 1 caractere especial, 1 letra maiúscula, 1 letra minúscula e 1 número"
+        />
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Loading..." : "Cadastrar"}
+        </button>
+      </form>
+
+      <div className={styles.switch_to_login}>
+        <Link href='login'>
+          <h1>Já tem uma conta? Faça login!</h1>
+        </Link>
+      </div>
+    </main>
+  )
+}
