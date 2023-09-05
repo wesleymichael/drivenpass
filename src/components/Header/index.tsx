dce213263ff973f2c90fa6a5a90ac06f02bef41e
Wesley Michael <wesleymichaelpereira@hotmail.com>
2023-09-05 02:31:21 -0300

    feat: Add logout functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { TbLogout } from 'react-icons/tb';
 import { GiPadlock } from 'react-icons/gi';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
+import { useUserContext } from '@/hooks/useUserContext';
 
 export function Header() {
+  const router = useRouter();
+  const { setUserData } = useUserContext();
+
+  function logout() {
+    const confirmLogout = confirm("Tem certeza de que deseja sair?");
+  
+    if (confirmLogout) {
+      localStorage.removeItem('userData');
+      setUserData({token: ''});
+      router.push('/login');
+    }
+  }
+  
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContext}>
@@ -12,7 +26,7 @@ export function Header() {
           <GiPadlock/>
         </Link>
         <Link href="/"><h1>DrivenPass</h1></Link>
-        <TbLogout/>
+        <TbLogout onClick={logout} />
       </div>
     </header>
   )
